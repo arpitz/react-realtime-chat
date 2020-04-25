d8e56598ebd6ada6e1d3fa53eea1bf9bef946a12
Arpit Trivedi <arpit1trivedi@gmail.com>
2020-04-25 18:12:40 +0530

    Added components and the HTML

--- react-expense-tracker/src/App.js
+++ react-expense-tracker/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Balance from './components/Balance';
+import IncomeExpense from './components/IncomeExpense';
+import TransactionList from './components/TransactionList';
+import AddTransaction from './components/AddTransaction';
 
 function App() {
   return (
     <div className="App">
-     Hello World
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpense/>
+        <TransactionList/>
+        <AddTransaction/>
+      </div>
     </div>
   );
 }
