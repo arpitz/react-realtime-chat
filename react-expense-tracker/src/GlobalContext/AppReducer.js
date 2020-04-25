3fe39bc05c0b1aa4ac0c82dbc80078c632b1b2ae
Arpit Trivedi <arpit1trivedi@gmail.com>
2020-04-25 19:36:11 +0530

    Add transaction code

--- react-expense-tracker/src/GlobalContext/AppReducer.js
+++ react-expense-tracker/src/GlobalContext/AppReducer.js
@@ -3,7 +3,11 @@ export const AppReducer = (state, action) => {
         case 'DELETE_TRANSACTION': return {
             ...state,
             transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
-        }
+        };
+        case 'ADD_TRANSACTION': return {
+            ...state,
+            transactions: [ action.payload, ...state.transactions ]
+        };
         default: return state;
     }  
 }
--- react-expense-tracker/src/GlobalContext/GlobalState.js
+++ react-expense-tracker/src/GlobalContext/GlobalState.js
@@ -22,10 +22,18 @@ export const GlobalProvider = ({ children }) => {
         });
     }
 
+    function addTransaction(transaction) {
+        dispatch({
+            type: 'ADD_TRANSACTION',
+            payload: transaction
+        })
+    }
+
     return (
         <GlobalContext.Provider value={{
             transactions: state.transactions,
-            deleteTransaction
+            deleteTransaction,
+            addTransaction
         }}>
             { children }
         </GlobalContext.Provider>
