72828a5072f4eae35739884d0d943450734b1a6d
Arpit Trivedi <arpit1trivedi@gmail.com>
2020-04-27 15:15:12 +0530

    Added users file

--- react-realtime-chat/client/src/components/Chat/Chat.js
+++ react-realtime-chat/client/src/components/Chat/Chat.js
@@ -14,8 +14,12 @@ const Chat = ({ location }) => {
         socket = io(ENDPOINT);
         setName(name);
         setRoom(room);
-        
+
         socket.emit('join', {name, room});
+        return () => {
+            socket.emit('disconnect');
+            socket.off();
+        }
     },[ ENDPOINT, location.search ])
 
     return(
