c67f0ad29f57254fad13f35a2b76172d48bc7476
Arpit Trivedi <arpit1trivedi@gmail.com>
2020-04-27 15:38:43 +0530

    Server side code for join and send message

--- react-realtime-chat/server/index.js
+++ react-realtime-chat/server/index.js
@@ -3,6 +3,7 @@ const socketio = require('socket.io');
 const http = require('http');
 
 const router = require('./router');
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
 const PORT = process.env.PORT || 5000;
 
@@ -11,10 +12,25 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on('connection', (socket) => {
-    console.log('We have a new connection !!!');
+    socket.on('join', ({ name, room }, callback) => {
+        const { error, user } = addUser({ id: socket.id, name, room });
+        if(error) return callback(error);
 
-    socket.on('join', ({ name, room }) => {
-        console.log(name, room);
+        // For the user who has newly joined the chat room
+        socket.emit('message', {user: 'admin', text: `${user.name}, welcome to the room ${user.room}`});
+        
+        // For all the other users who are already in that room
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!`});
+
+        socket.join(user.room);
+        callback();
+    })
+
+    // When a user sends a message in the chat room.
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+        io.to(user.room).emit('message', { user: user.name, text: message });
+        callback();
     })
     
     socket.on('disconnect', () => {
